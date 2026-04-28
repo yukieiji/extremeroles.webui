c8122efb29b3299ee5f1d2a99f78befb87975b8b
yukieiji <40425798+yukieiji@users.noreply.github.com>
2026-04-28 18:36:29 +0900

    remove: use

--- src/feature/rightsidepanel/AuTab0Viewer.tsx
+++ src/feature/rightsidepanel/AuTab0Viewer.tsx
@@ -1,6 +1,4 @@
-import { use } from "react";
 import { auOptionMetaData } from "../../logics/api";
-import { getAllOptions } from "../../logics/api.store";
 import { AuTab0GeneralCategory } from "./AuTab0GeneralCategory";
 import { AuTab0MapCategory } from "./AuTab0MapCategory";
 
@@ -8,7 +6,6 @@ import { AuTab0MapCategory } from "./AuTab0MapCategory";
  * Auのタブ0の設定内容を表示し、ダブルクリックで該当箇所へ移動するコンポーネント
  */
 export function AuTab0Viewer() {
-	use(getAllOptions());
 	const tab0CategoryIds = auOptionMetaData.tabCategoryMap[0] || [];
 
 	return (
