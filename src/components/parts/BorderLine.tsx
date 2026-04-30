4040d1ddc7f0df361cfaeb8b457ee6da975b1d85
yukieiji <40425798+yukieiji@users.noreply.github.com>
2026-04-30 13:00:31 +0900

    fix: style

--- src/components/parts/BorderLine.tsx
+++ src/components/parts/BorderLine.tsx
@@ -1,3 +1,3 @@
 export function BorderLine() {
-	return <hr className="w-[95%] mx-auto border-t border-r-2 border-gray-700" />;
+	return <hr className="w-[95%] mx-auto border-t rounded-lg border-gray-700" />;
 }
