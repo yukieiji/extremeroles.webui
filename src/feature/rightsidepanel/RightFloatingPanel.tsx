0351e418a4eec7b8658a47827a355f65e50efb0c
yukieiji <40425798+yukieiji@users.noreply.github.com>
2026-05-03 16:23:37 +0900

    refactor

--- src/feature/rightsidepanel/RightFloatingPanel.tsx
+++ src/feature/rightsidepanel/RightFloatingPanel.tsx
@@ -1,5 +1,5 @@
 import { use, useEffect } from "react";
-import { CompactAccordion } from "../../components/blocks/CompactAccordion";
+import { VieweGroupAccordion } from "../../components/blocks/VieweGroupAccordion";
 import { RightPanelGroupColumnLayout } from "../../components/parts/RightPanelGroupColumnLayout";
 import { getAllOptions } from "../../logics/api.store";
 import {
@@ -85,28 +85,28 @@ export function RightFloatingPanel() {
 						<h2 className="text-lg font-semibold">{RIGHT_PANEL_TITLE}</h2>
 					</div>
 					<div className="flex-1 overflow-y-auto p-3">
-						<CompactAccordion
+						<VieweGroupAccordion
 							title={SETTING_VALUES_TITLE}
 							isOpen={isSettingsOpen}
 							onToggle={toggleSettings}
 						>
 							<RightPanelGroupColumnLayout>
-								<CompactAccordion
+								<VieweGroupAccordion
 									title={AU_SETTINGS_TITLE}
 									isOpen={isAuSettingsOpen}
 									onToggle={toggleAuSettings}
 								>
 									<AuOptionViewer />
-								</CompactAccordion>
-								<CompactAccordion
+								</VieweGroupAccordion>
+								<VieweGroupAccordion
 									title={EXR_SETTINGS_TITLE}
 									isOpen={isExrSettingsOpen}
 									onToggle={toggleExrSettings}
 								>
 									<ExROptionViewer />
-								</CompactAccordion>
+								</VieweGroupAccordion>
 							</RightPanelGroupColumnLayout>
-						</CompactAccordion>
+						</VieweGroupAccordion>
 					</div>
 				</div>
 			</aside>
