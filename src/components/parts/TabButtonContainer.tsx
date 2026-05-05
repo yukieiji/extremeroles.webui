3758a759f36297589d64ecbbe922732db58db9a8
yukieiji <40425798+yukieiji@users.noreply.github.com>
2026-05-06 00:08:47 +0900

    fix: tab style

--- src/components/parts/TabButtonContainer.tsx
+++ src/components/parts/TabButtonContainer.tsx
@@ -6,7 +6,7 @@ interface TabButtonContainerProp {
 
 export function TabButtonContainer({ children }: TabButtonContainerProp) {
 	return (
-		<div className="flex flex-wrap gap-2 border-b-4 border-gray-200">
+		<div className="pt-1 px-2 grid grid-cols-4 gap-2.5 border-b-4 border-gray-200">
 			{children}
 		</div>
 	);
