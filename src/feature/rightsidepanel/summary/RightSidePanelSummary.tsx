3feca36c3d46e11027edb560ecd6cca3f486406c
yukieiji <40425798+yukieiji@users.noreply.github.com>
2026-06-14 11:29:48 +0900

    remove: bg

--- src/feature/rightsidepanel/summary/RightSidePanelSummary.tsx
+++ src/feature/rightsidepanel/summary/RightSidePanelSummary.tsx
@@ -25,7 +25,7 @@ import { VanillaRoleSummaryRow } from "./VanillaRoleSummaryRow";
 export function RightSidePanelSummary() {
 	return (
 		<div
-			className="flex flex-col gap-1 p-3 border-b border-gray-200 bg-gray-50"
+			className="flex flex-col gap-1 p-3 border-b border-gray-200"
 			data-testid="right-panel-summary"
 		>
 			<PresetSummaryRow />
