721f51a713b51cfeb562b15bbfb733722ad9ce53
yukieiji <40425798+yukieiji@users.noreply.github.com>
2026-07-05 00:54:05 +0900

    fix: localhost

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig(({ mode }) => ({
     proxy: {
       '/exr/option/': `http://localhost:${port}`,
       '/exr/role/filter/': `http://localhost:${port}`,
+      '/exr/role/simulate/': `http://localhost:${port}`,
       '/au/option/': `http://localhost:${port}`,
       '/au/translation/batch/': `http://localhost:${port}`,
       '/au/translation/batch/optionunit/': `http://localhost:${port}`,
